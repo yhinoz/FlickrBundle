32f89438032c52a79d1e718f718c6fe32e4e162d
yhino <gyhino@gmail.com>
2012-04-17 19:58:20 +0900

    modified testsuites

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,10 @@
 <?php
+$autoload = __DIR__ . '/vendor/.composer/autoload.php';
+if (!file_exists($autoload)) {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+require $autoload;
 
+spl_autoload_register(function($class)
+{
+});
